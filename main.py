65a8e675e8a4b43c50f80d33c286f1457a3ecd79
shourya-r <rajputshourya7@gmail.com>
2022-12-23 17:29:24 +0530

    k

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ cap = cv2.VideoCapture(0)
 while True:
     #Reading the frame
     ret, frame = cap.read()
-
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray = np.float32(gray)
     # Converting the frame to the HSV color space
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -22,6 +23,8 @@ while True:
     mask = cv2.erode(mask, kernel, iterations=1)
     mask = cv2.dilate(mask, kernel, iterations=1)
 
+    corners = cv2.goodFeaturesToTrack(mask,20,0.1,10)
+
     # Find contours in the mask
     # Contours -  Curve joining all the continuous points (along the boundary), having same color or intensity.
     contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # arguments - first one is source image, second is contour retrieval mode, third is contour approximation method
@@ -33,11 +36,11 @@ while True:
         # Check the contour size
         if cv2.contourArea(i) <500:  #Ends the iteration for small areas to avoid false positives
             continue
-
+        if len(corners) >=7 :
         # Draw a bounding box around the contour using boundingRect
         # create an approximate rectangle along with the image
-        x, y, w, h = cv2.boundingRect(i)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  #arg- image, starting point,ending point, color,thickness
+            x, y, w, h = cv2.boundingRect(i)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  #arg- image, starting point,ending point, color,thickness
 
     # Display the resulting frame
     cv2.imshow('Frame', frame)
